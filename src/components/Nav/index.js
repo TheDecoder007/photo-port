04baf6ae41f79f0c10b82b102483584ada2268d2
Ryan Murphy <TheDecoder007@gmail.com>
2022-08-03 14:05:15 -0400

    testing code is there, but packages wouldn't download

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -19,7 +19,7 @@ const Nav = () => {
   return (
     <header>
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
             📸
           </span>{" "}
@@ -29,7 +29,7 @@ const Nav = () => {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About Me</a>
+            <a data-testid="about" href="#about">About Me</a>
           </li>
           <li>
             <span>Contact</span>
